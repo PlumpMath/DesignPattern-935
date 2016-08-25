bfa2faaab359b9b37fbce6d39cf6cc15af7a786e
cairo <zhanghaoyu10@gmail.com>
2016-08-25 13:05:23 +0800

    单例模式

---
+++ Singleton/Configer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Configer
+    {
+        private static volatile Configer _configer = null;
+        private static object _lock = new object();
+        private Configer() { }
+
+        public static Configer getInstance()
+        {
+            if (_configer == null)
+            {
+                lock(_lock)
+                {
+                        if (_configer == null)
+                        { 
+                            _configer = new Configer();
+                        }
+                }
+                
+            }
+            return _configer;
+        }
+
+        public void getConfig()
+        {
+            Console.WriteLine("I am a singeton object and i am reading config");
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Configer m_configer = Configer.getInstance();
+            m_configer.getConfig();
+            Console.ReadKey();
+        }
+    }
+}
