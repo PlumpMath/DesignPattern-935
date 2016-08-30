ea8108c7baf55db4d0150135c44e3b8b65d40071
cairo <zhanghaoyu10@gmail.com>
2016-08-30 17:12:27 +0800

    适配器模式

---
+++ AdapterPattern/Adapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public class Adapter : NewStringCompatible
+    {
+        private ClassicString mString;
+        public Adapter(string a)
+        { 
+            this.mString = new ClassicString(a);
+        }
+        public override string getNewString()
+        {
+            return mString.getClassicString();
+        }
+    }
+}
---
+++ AdapterPattern/ClassicString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public class ClassicString
+    {
+        private string _internString = null;
+
+        public ClassicString(string a)
+        {
+            _internString = a;
+        }
+        public string getClassicString()
+        {
+            return _internString;
+        }
+    }
+}
